17540f6bca23b84a3192534b685328850af42909
surajshanbhag <smshanbh@ncsu.edu>
2016-08-25 00:24:38 -0400

    Updated Code: Removed local graphics , pena nd brushes

--- AdvancedRadar/AdvancedRadar/Radar.cs
+++ AdvancedRadar/AdvancedRadar/Radar.cs
@@ -14,7 +14,9 @@ namespace AdvancedRadar
     {
         // Colors
         private Color color_Background = Color.Black;
-        private Color color_Line = Color.GreenYellow;
+        private Color color_Circle = Color.GreenYellow;
+        private Color color_Line = Color.Green;
+        private Color color_SweepLine = Color.Aqua;
         private Color color_Point = Color.Red;
 
         // pen sizes
@@ -38,7 +40,7 @@ namespace AdvancedRadar
         private static int number_Radar = new int();
         private string name_Radar;
 
-        private int angle_SweepLine;
+        private float angle_SweepLine;
         private int index_Data;
 
         private int divisions_Angle;    // total number of divisions of 360 degrees
@@ -50,6 +52,9 @@ namespace AdvancedRadar
         private Bitmap bmp_Radar;
         private Bitmap bmp_BaseRadar;
         private Graphics graphics_Radar;
+        private Pen pen_Radar;
+        private Brush brush_RadarBackground;
+        private Brush brush_Point;
         /*-------------------------------------------------------------*/
         public Radar(Form form, int size, int x, int y, int Divisions, string name = "Radar")
         {
@@ -87,61 +92,63 @@ namespace AdvancedRadar
             bmp_BaseRadar = new Bitmap(size_PictureBox, size_PictureBox);
             pictureBox_Radar.Image = bmp_Radar;
 
-            draw_Radar();
+            graphics_Radar = Graphics.FromImage(bmp_Radar);
+            brush_RadarBackground = new SolidBrush(color_Background);
+            brush_Point = new SolidBrush(color_Point);
+            pen_Radar = new Pen(Color.GreenYellow, penSz_Circle);
+            draw_Radar(bmp_Radar);
             form.ResumeLayout();
 
         }
-        public void update_Global(int angle, int value)
+        public void update_Global(Form form, int angle, int value)
         {
+            form.SuspendLayout();
+
             data_Global[angle] = value * length_SweepLine / 100;
 
-            Bitmap bmp = new Bitmap(bmp_BaseRadar);
-
-            angle_SweepLine = (int)(360 * angle / this.divisions_Angle);
-
-            Draw_Line(angle_SweepLine, length_SweepLine, Color.Aqua, bmp);
+            draw_Radar(bmp_Radar);
+            angle_SweepLine = (float)(float)(360 * (float)angle / (float)this.divisions_Angle);
+            index_Data = angle;
+            Draw_Line(angle_SweepLine, length_SweepLine, color_SweepLine, bmp_Radar);
 
             for (int index = 0; index < divisions_Angle; index++)
             {
                 if (data_Covered[index] != 0 && index != angle_SweepLine)
                 {
-                    Draw_point(index, data_Covered[index], color_Point, bmp, false);
+                    float tempAngle = (float)(float)(360 * (float)index / (float)this.divisions_Angle);
+                    Draw_point(tempAngle, data_Covered[index], color_Point, bmp_Radar, false);
                 }
             }
-            Draw_point(angle_SweepLine, data_Global[angle_SweepLine], color_Point, bmp, false);
-            data_Covered[angle_SweepLine] = data_Global[angle_SweepLine];
+            Draw_point(angle_SweepLine, data_Global[index_Data], color_Point, bmp_Radar, false);
+            data_Covered[index_Data] = data_Global[index_Data];
             //load bitmap in picturebox1
-            pictureBox_Radar.Image = bmp;
-
+            pictureBox_Radar.Image = bmp_Radar;
+            form.ResumeLayout();
 
         }
-        public void draw_Radar()
+        public void draw_Radar(Bitmap bmp)
         {
-            Draw_Circle(size_Radar / 2, true, bmp_BaseRadar);
-            Draw_Circle((int)(0.75 * size_Radar / 2), true, bmp_BaseRadar);
-            Draw_Circle((int)(0.50 * size_Radar / 2), true, bmp_BaseRadar);
-            Draw_Circle((int)(0.25 * size_Radar / 2), true, bmp_BaseRadar);
-            Draw_Line(0, length_SweepLine, color_Line, bmp_BaseRadar);
-            Draw_Line(90, length_SweepLine, color_Line, bmp_BaseRadar);
-            Draw_Line(180, length_SweepLine, color_Line, bmp_BaseRadar);
-            Draw_Line(270, length_SweepLine, color_Line, bmp_BaseRadar);
+            Draw_Circle(size_Radar / 2, true, bmp);
+            Draw_Circle((int)(0.75 * size_Radar / 2), true, bmp);
+            Draw_Circle((int)(0.50 * size_Radar / 2), true, bmp);
+            Draw_Circle((int)(0.25 * size_Radar / 2), true, bmp);
+            Draw_Line(0, length_SweepLine, color_Line, bmp);
+            Draw_Line(90, length_SweepLine, color_Line, bmp);
+            Draw_Line(180, length_SweepLine, color_Line, bmp);
+            Draw_Line(270, length_SweepLine, color_Line, bmp);
         }
         public void Draw_Circle(int radius, bool fill, Bitmap bmp)
         {
-            Pen myPen = new Pen(color_Line, penSz_Circle);
-            Graphics myGraphics = Graphics.FromImage(bmp);
             if (fill)
             {
-                SolidBrush myBrush = new System.Drawing.SolidBrush(color_Background);
-                myGraphics.FillEllipse(myBrush, xloc_Radar - radius, yloc_Radar - radius, radius * 2, radius * 2);
-                myBrush.Dispose();
+                graphics_Radar.FillEllipse(brush_RadarBackground, xloc_Radar - radius, yloc_Radar - radius, radius * 2, radius * 2);
             }
-
-            myGraphics.DrawEllipse(myPen, xloc_Radar - radius, yloc_Radar - radius, radius * 2, radius * 2);
-            myGraphics.Dispose();
-            myPen.Dispose();
+            pen_Radar.Color = color_Circle;
+            pen_Radar.Width = penSz_Circle;
+            graphics_Radar.DrawEllipse(pen_Radar, xloc_Radar - radius, yloc_Radar - radius, radius * 2, radius * 2);
+            //myGraphics.Dispose();
         }
-        public void Draw_point(int angle, int distance, Color colorPoint, Bitmap bmp, bool line = true)
+        public void Draw_point(float angle, int distance, Color colorPoint, Bitmap bmp, bool line = true)
         {
             int radius = 1;
             int center_x = 0;
@@ -151,7 +158,6 @@ namespace AdvancedRadar
             {
                 //right half
                 //u in degree is converted into radian.
-
                 center_x = xloc_Radar + (int)(distance * Math.Sin(Math.PI * angle / 180));
                 center_y = yloc_Radar - (int)(distance * Math.Cos(Math.PI * angle / 180));
             }
@@ -160,27 +166,19 @@ namespace AdvancedRadar
                 center_x = xloc_Radar - (int)(distance * -Math.Sin(Math.PI * angle / 180));
                 center_y = yloc_Radar - (int)(distance * Math.Cos(Math.PI * angle / 180));
             }
-            Pen myPen = new Pen(colorPoint, penSz_Point);
-            SolidBrush myBrush = new System.Drawing.SolidBrush(colorPoint);
-            Graphics myGraphics = Graphics.FromImage(bmp);
-            myGraphics.DrawEllipse(myPen, center_x - radius, center_y - radius, radius * 2, radius * 2);
-            myGraphics.FillEllipse(myBrush, center_x - radius, center_y - radius, radius * 2, radius * 2);
+            pen_Radar.Color = color_Point;
+            pen_Radar.Width = penSz_Point;
+            graphics_Radar.DrawEllipse(pen_Radar, center_x - radius, center_y - radius, radius * 2, radius * 2);
+            graphics_Radar.FillEllipse(brush_Point, center_x - radius, center_y - radius, radius * 2, radius * 2);
             if (line)
             {
                 Draw_Line(angle, distance, color_Point, bmp);
             }
-            myGraphics.Dispose();
-            myPen.Dispose();
-            myBrush.Dispose();
         }
-        public void Draw_Line(int angle, int length, Color linecolor, Bitmap bmp)
+        public void Draw_Line(float angle, int length, Color linecolor, Bitmap bmp)
         {
             int x = 0;
             int y = 0;
-            if (angle == -1)
-            {
-                angle = 359;
-            }
             if (angle >= 0 && angle <= 180)
             {
                 //right half
@@ -194,11 +192,10 @@ namespace AdvancedRadar
                 x = xloc_Radar - (int)(length * -Math.Sin(Math.PI * angle / 180));
                 y = yloc_Radar - (int)(length * Math.Cos(Math.PI * angle / 180));
             }
-            Pen myPen = new Pen(linecolor, penSz_SweepLine);
-            Graphics myGraphics = Graphics.FromImage(bmp);
-            myGraphics.DrawLine(myPen, new Point(xloc_Radar, yloc_Radar), new Point(x, y));
-            myGraphics.Dispose();
-            myPen.Dispose();
+            pen_Radar.Color = linecolor;
+            pen_Radar.Width = penSz_SweepLine;
+            graphics_Radar.DrawLine(pen_Radar, new Point(xloc_Radar, yloc_Radar), new Point(x, y));
+
         }
     }
 }
